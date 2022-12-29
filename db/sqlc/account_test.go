e4f70b6f6938ba3fb62062caf43d3e919fac5bc0
LonghamBridge <46768957+LonghamBridge@users.noreply.github.com>
2022-12-28 21:49:22 -0500

    update

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/longhambridge/simple_bank/util"
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
 )
 
 func createRandomAccount(t *testing.T) Account {
